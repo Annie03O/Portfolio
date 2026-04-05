ddf65b3729713efed00df800ef05ad38bbfec150
Annie Olofsson <annielolofsson@gmail.com>
2026-04-05 17:06:14 +0200

    Start building project-section

--- app/components/Home/Home.tsx
+++ app/components/Home/Home.tsx
@@ -5,7 +5,7 @@ import ProjectsPreview from "./ProjectsPreview";
 
 const Home = () => {   
    return (
-     <section className="flex text-black justify-center w-full h-full">
+     <section className="flex text-black justify-center w-full h-screen">
       <section className="flex flex-col items-center justify-center w-full h-full">
          <Banner/>
          <ProjectsPreview/>
